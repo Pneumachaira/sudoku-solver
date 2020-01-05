8f49fa6baffe23de01837557f0a8f753c9ebe73f
Jacob Schuster <46460639+Pneumachaira@users.noreply.github.com>
2020-01-04 23:42:59 -0500

    guess() update
    Sudoku Solver 3.0! Includes looking for Sole Candidate and Unique Candidate, as well as targeted brute-forcing when the first two are no longer useful.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ listRow = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 listBox = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 listSole = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 listUnique = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+listGuess = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 #This function prints out the Sudoku format to make sure user input was correct
 #It also will print out the completed Sudoku at the end!
@@ -193,8 +194,6 @@ def checkBlockUnique(i): #Where i is the horizontal value of blocks we're checki
                 continue
             #Check the columns!
             for y in range (0, 9): #Where y is the vertical value of the column
-                if k == 7 and i == 0 and j == 0:
-                    print(sudokuPuzzle[(i * 3) + (y * 9) + 1])
                 if sudokuPuzzle[(i * 3) + (y * 9)] == str(k):
                     listUnique[0] = 0
                     listUnique[3] = 0
@@ -223,40 +222,171 @@ def checkBlockUnique(i): #Where i is the horizontal value of blocks we're checki
             sumUnique = 0
             for z in range(0, 9): #Where z just goes through the list
                 sumUnique = sumUnique + listUnique[z]
-            if k == 7 and i == 0 and j == 0:
-                print(sumUnique)
             if (sumUnique == 1 and openSpot == 1): #If only one spot can be found for m
                 sudokuPuzzle[(j * 27) + (i * 3) + (9 * (listUnique.index(1) // 3)) + (listUnique.index(1) % 3)] = str(k)
             for z in range (0, 9):
                 listUnique[z] = 1 #Reset the list
 
-#MAIN FUNCTION
-#MAIN FUNCTION
-#MAIN FUNCTION
-#MAIN FUNCTION
-#MAIN FUNCTION
-#MAIN FUNCTION
-#MAIN FUNCTION
-#MAIN FUNCTION
+#This function determines the most influential spots (blocks with most openings), and then plugs numbers in.
+#The Sudoku will be run through with the plugged-in number, and if it cannot be completed, we try another
+def guess():
+    for i in range(0, 81): #This part saves a backup of the Sudoku so that we can restore it if necessary
+        sudokuBackup.append(sudokuPuzzle[i])
+    #This part sees which block is most open
+    guessFinal = 0
+    for i in range(0, 3): #Horizontal value of 3x3 block
+        for j in range(0, 3): #Vertical value of 3x3 block
+            guessTemp = 0
+            for m in range(0, 2): #Row of cells in block
+                if sudokuPuzzle[(i * 3) + (j * 27) + (m * 9)] == '':
+                    guessTemp = guessTemp + 1
+                if sudokuPuzzle[(i * 3) + (j * 27) + (m * 9) + 1] == '':
+                    guessTemp = guessTemp + 1
+                if sudokuPuzzle[(i * 3) + (j * 27) + (m * 9) + 2] == '':
+                    guessTemp = guessTemp + 1
+            if guessTemp >= guessFinal:
+                guessFinal = guessTemp
+                guessX = i
+                guessY = j
+    #This part sees which cell is the most influential (has most vertical/horizontal empty neighbors)
+    for m in range (0, 3): #Row of cell in block
+        for n in range (0, 3): #Column of cell in block
+            x = (guessX * 3) + (guessY * 27) + (m * 9) + n
+            if sudokuPuzzle[x] == '': #Don't bother with cells already filled in
+                #So first we gotta check the vertical
+                while (x >= 0): #Checks earlier list items (above)
+                    if sudokuPuzzle[x] == '':
+                        listGuess[(m * 3) + n] += 1
+                    x = x - 9
+                x = (guessX * 3) + (guessY * 27) + (m * 9) + n
+                x = x + 9
+                while (x <= 80): #Checks later list items (below)
+                    if sudokuPuzzle[x] == '':
+                        listGuess[(m * 3) + n] += 1
+                    x = x + 9
+                #Now check the horizontal
+                x = (guessX * 3) + (guessY * 27) + (m * 9) + n
+                y = 8
+                while (y >= x % 9): #Checks to the left, the y keeps it from looping around
+                    if sudokuPuzzle[x] == '':
+                        listGuess[(m * 3) + n] += 1
+                    y = x % 9
+                    x = x - 1
+                x = (guessX * 3) + (guessY * 27) + (m * 9) + n
+                x = x + 1
+                while (x % 9 > 0): #Checks to the right
+                    if sudokuPuzzle[x] == '':
+                        listGuess[(m * 3) + n] += 1
+                    x = x + 1
+    #Now that we've got values for empty neighbors, we can see which one has the most
+    guessTemp = 0
+    for i in range(0, 8):
+        if listGuess[i] > guessTemp:
+            guessTemp = listGuess[i]
+    guessFinal = listGuess.index(guessTemp) #We now have the index of the corresponding best cell
+    #Now we plug in a number and see if we can get to the end
+    for k in range(1, 10): #Where k is the plugged-in number
+        for i in range(0, 81): #Here we reset the Sudoku to pre-guess state
+            sudokuPuzzle[i] = sudokuBackup[i]
+        sudokuPuzzle[(guessX * 3) + (guessY * 27) + (guessFinal // 3 * 9) + (guessFinal % 3)] = str(k)
+        print('We\'ll put a ' + str(k) + ' in there.')
+        for i in range(0, 81, 9):
+            printSudoku(i)
+        while True:
+            for i in range(0, 81):
+                sudokuOld[i] = sudokuPuzzle[i]
 
-
+            input()
     
+            print('checkSole Guess')
+    
+            for i in range(0, 81): #Sole Candidate search
+                checkCol(i)
+                checkRow(i)
+                checkBlock(i)
+                checkSole(i)
+            
+            for i in range(0, 81, 9): #Progress report
+                printSudoku(i)
+
+            input()
+            print('checkColUnique Guess')
+
+            for i in range(0, 9): #Unique Candidate search
+                checkColUnique(i)
+
+            for i in range(0, 81, 9): #Progress report
+                printSudoku(i)
+
+            input()
+            print('checkRowUnique Guess')
+
+            for i in range(0, 9):
+                checkRowUnique(i)
+
+            for i in range(0, 81, 9): #Progress report
+                printSudoku(i)
+
+            input()
+            print('checkBlockUnique Guess')
+        
+            for i in range(0, 3):
+                checkBlockUnique(i)
+
+            for i in range(0, 81, 9): #Progress report
+                printSudoku(i)
+
+            complete = 1
+            for i in range(0, 81): #This part checks to see if the puzzle is complete
+                if (sudokuPuzzle[i] == ''):
+                    complete = 0
+            if (complete == 1):
+                break
+
+            if sudokuOld == sudokuPuzzle:
+                print('Hrm.')
+                print('Let\'s go back and try a different number!')
+                break
+        if complete == 1:
+            break
+
+#MAIN FUNCTION
+#MAIN FUNCTION
+#MAIN FUNCTION
+#MAIN FUNCTION
+#MAIN FUNCTION
+#MAIN FUNCTION
+#MAIN FUNCTION
+#MAIN FUNCTION
+
+
+#First we need to input the Sudoku puzzle   
 sudokuPuzzle = list()
+sudokuOld = list()
+sudokuBackup = list()
 print('Please enter the values of the Sudoku puzzle one by one, row by row.')
 print('For blank spots, hit "Enter" without entering a value.')
 while len(sudokuPuzzle) != 81:
     sudokuPuzzle.append(input())
 
-#Below is a pre-set Sudoku puzzle
+#Below are pre-set Sudoku puzzles
+#Easy:
 #sudokuPuzzle = ['5', '', '', '', '', '', '', '', '8', '', '', '', '8', '2', '', '3', '', '', '6', '8', '2', '5', '', '', '', '', '', '', '1', '', '', '', '', '', '9', '', '', '', '', '4', '3', '8', '6', '', '1', '', '6', '5', '7', '', '', '8', '', '2', '7', '9', '', '2', '', '6', '', '', '4', '', '', '3', '1', '', '7', '', '', '', '8', '', '', '', '', '', '', '', '']
+#Very Hard (mostly complete, was for guess() testing):
+#sudokuPuzzle = ['', '', '', '2', '4', '6', '1', '5', '7', '2', '7', '5', '8', '1', '3', '6', '9', '4', '4', '6', '1', '9', '7', '5', '2', '8', '3', '', '', '', '4', '', '', '8', '', '', '', '', '4', '6', '', '8', '7', '', '', '', '', '8', '3', '5', '', '4', '', '', '1', '9', '2', '5', '6', '4', '3', '7', '8', '', '', '6', '7', '3', '2', '9', '4', '1', '3', '4', '7', '1', '8', '9', '5', '6', '2']
 
 for i in range(0, 81, 9):
     printSudoku(i)
-print('Is this correct? If not, restart the program. Otherwise, hit Enter to start,')
+print('Is this correct? If not, restart the program. Otherwise, hit Enter to start.')
 input()
 while True:
-    complete = 1
-
+    if len(sudokuOld) != 81:
+        for i in range(0, 81):
+            sudokuOld.append(sudokuPuzzle[i])
+    else:
+        for i in range(0, 81):
+            sudokuOld[i] = sudokuPuzzle[i]
+    
     print('checkSole')
     
     for i in range(0, 81): #Sole Candidate search
@@ -297,9 +427,17 @@ while True:
         
     input()
 
+    if sudokuOld == sudokuPuzzle:
+        print('There is nothing more I can do for you.')
+        print('Time to start guessing!')
+        guess()
+
+    complete = 1
     for i in range(0, 81): #This part checks to see if the puzzle is complete
         if (sudokuPuzzle[i] == ''):
             complete = 0
     if (complete == 1):
         print('Success!!!!!!')
+        for z in range(0, 81, 9):
+            printSudoku(z)
         break
